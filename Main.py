07fb44ac434ee48643766f625b12ab4d67b30b53
Clifford Danza <cliff.danza.dev@gmail.com>
2021-10-14 11:13:05 -0700

    Removed old preprocess, added and implemented separated preprocessing functions

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import nltk
-from Preprocess import full_preprocess, lemmatizer, clean_file, lemmatizer_verify
+from Preprocess import remove_punct, to_lowercase, remove_stopwords, lemmatize, lemmatizer_verify
 from Figures import word_hifreq_chart, word_lofreq_chart
 
 """
@@ -9,28 +9,34 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 """
+# -----------------------------------------------------------------------------------------------
 
 # Opening and reading the file
 f = open('/Users/cdanza/Documents/Coding/TheHobbitNLP/TheHobbit.txt','r', encoding='utf8')
 raw = f.read()
 
+# -----------------------------------------------------------------------------------------------
 
-# Still testing, but will possibly remove: Tokenization, cleaning, lemmatizing in that order -> problem due to stopwords created post-lemma
-# New idea to create functions for each step, then apply in whichever order chosen
-wordtokenized3 = nltk.word_tokenize(raw)
-clean_corpus = clean_file(wordtokenized3)
-lemmatized_corpus = lemmatizer(clean_corpus)
+# Still testing -> adverbs not always lemmatizing, dwarves = 32
+tok = nltk.word_tokenize(raw)
+rp = remove_punct(tok)
+lo = to_lowercase(rp)
+rs = remove_stopwords(lo)
+lm = lemmatize(rs)
 
-# Still testing -> adverbs not always lemmatizing
-fullpreprocess = full_preprocess(raw)
+lemmatizer_verify('dwarf', 'dwarves', lm, rs)
 
-lemmatizer_verify('quick', 'quickly', lemmatized_corpus, fullpreprocess)
+# -----------------------------------------------------------------------------------------------
 
-# Get charts
-#word_hifreq_chart(fullpreprocess)
-#word_hifreq_chart(lemmatized_corpus)
-#word_lofreq_chart(lemmatized_corpus)
+# Display data
 
+"""
+word_hifreq_chart(lm)
+word_hifreq_chart(rs)
+# word_lofreq_chart(lemmatized_corpus)
+"""
+
+# -----------------------------------------------------------------------------------------------
 
 """
 Future directions:
--- Preprocess.py
+++ Preprocess.py
@@ -9,60 +9,10 @@ stopwords = nltk.corpus.stopwords.words('english')
 custom_stopwords = ['say', 'get', 'one', 'could', 'like']
 stopwords.extend(custom_stopwords)
 
-
-"""
-# Should fully preprocess, need to continue testing
-# Considering breaking into 4 separate functions
-def full_preprocess(file) -> list:
-    
-    wordtokenized = nltk.word_tokenize(file)
-    
-    remove_punct = [word for word in wordtokenized if word.isalnum()]
-    to_lowercase = [word.lower() for word in remove_punct]
-    
-    
-    # Pos tagger function
-    def pos_tagger(nltk_tag):
-    	if nltk_tag.startswith('J'):
-    		return wordnet.ADJ
-    	elif nltk_tag.startswith('V'):
-    		return wordnet.VERB
-    	elif nltk_tag.startswith('N'):
-    		return wordnet.NOUN
-    	elif nltk_tag.startswith('R'):
-    		return wordnet.ADV
-    	else:		
-    		return None
-    
-    # Find the pos tag for each token
-    pos_tagged = nltk.pos_tag(to_lowercase)
-    
-    # Use our made pos_tagger to simplify
-    wordnet_tagged = list(map(lambda x: (x[0], pos_tagger(x[1])), pos_tagged))
-    
-    lemmatized_corpus = []
-    
-    for word, tag in wordnet_tagged:
-        if tag is None:
-    		# If there is no available tag, append the token as is
-            lemmatized_corpus.append(word)
-        else:
-    		# else use the tag to lemmatize the token
-            lemmatized_corpus.append(wnl.lemmatize(word, tag))
-
-    # Recreates one long string out of wordnet_tagged   
-    lemmatized_corpus = " ".join(lemmatized_corpus)
-    # Retokenize
-    wordtokenized2 = nltk.word_tokenize(lemmatized_corpus)
-    
-    remove_stopwords = [word for word in wordtokenized2 if word not in stopwords]
-
-    return remove_stopwords
-"""
-
 # -----------------------------------------------------------------------------------------------
 
 # Each cleaning task separated by function
+# Adverbs still not lemmatizing.
 def remove_punct(file) -> list:
     r = [word for word in file if word.isalnum()]
     
@@ -106,6 +56,9 @@ def lemmatize(file):
         if tag is None:
     		# If there is no available tag, append the token as is
             lemmatized_corpus.append(word)
+        # This doesn't seem to work, theres' still 32 dwarves occurrences
+        elif tag == "dwarves":
+            lemmatized_corpus.append("dwarf")
         else:	
     		# else use the tag to lemmatize the token
             lemmatized_corpus.append(wnl.lemmatize(word, tag))
@@ -117,7 +70,7 @@ def lemmatize(file):
 
 # -----------------------------------------------------------------------------------------------
 
-# Load in 2 strings to compare, then 2 lists. For me: List1 clean_corpus, List2 lemmatized_corpus
+# Load in 2 strings to compare, then 2 lists to compare relative string count pre- and post- lemmatization
 def lemmatizer_verify(w, x, y, z):
     y_count_w = 0
     y_count_x = 0
